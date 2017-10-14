0698bc13c6cc4087520ed368cd2de445c2aa3544
chhavi-au <chhavi.negi.au@gmail.com>
2017-10-15 01:30:04 +1100

    Update email settings

--- config/config.js
+++ config/config.js
@@ -4,5 +4,6 @@
   "baseUrl"         : "http://localhost:5000/",
   "apiUrl"          : "http://localhost:5000/api",
   "session_secret"  : "klugsuit57v6t765975978b6ov6bo8768b76o86bo87uig",
-  "lang"            : "en"
+  "lang"            : "en",
+  "email_from"      : "aipassignment@gmail.com"
 }
