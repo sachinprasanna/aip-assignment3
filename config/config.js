c166d0e9b4d2080932b821b5eae9449912ea9608
chhavi-au <chhavi.negi.au@gmail.com>
2017-10-15 22:13:18 +1100

    update api url

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 ﻿module.exports = {
   "port"            : 5000,
-  "connectionString": "mongodb://aipassignment3:<bi74igsdfiyt876tg>@ds113775.mlab.com:13775/heroku_644ndmcl",
+  "connectionString": "mongodb://aipassignment3:bi74igsdfiyt876tg@ds113775.mlab.com:13775/heroku_644ndmcl",
   "apiUrl"          : "https://blooming-fjord-52237.herokuapp.com/api",
   "session_secret"  : "klugsuit57v6t765975978b6ov6bo8768b76o86bo87uig",
   "lang"            : "en",
