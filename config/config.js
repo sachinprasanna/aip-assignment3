b7ed75c0061c060f3cc87e051be94be087219d4a
chhavi-au <chhavi.negi.au@gmail.com>
2017-10-16 17:33:33 +1100

    Fix for app url

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 ﻿module.exports = {
   "port"            : 5000,
-  "connectionString": "mongodb://aipassignment3:bi74igsdfiyt876tg@ds113775.mlab.com:13775/heroku_644ndmcl",
-  "apiUrl"          : "https://blooming-fjord-52237.herokuapp.com/api",
+  "connectionString": "mongodb://localhost:27017/mydiary",
+  "apiUrl"          : "http://localhost:5000/api",
   "session_secret"  : "klugsuit57v6t765975978b6ov6bo8768b76o86bo87uig",
   "lang"            : "en",
   "email_from"      : "aipassignment@gmail.com"
